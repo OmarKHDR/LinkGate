ccc7e01ad539a21062173d313aa68a6e40e496b6
OmarKHDR <omarsliman37@gmail.com>
2026-02-01 13:39:34 +0200

    adding dto for urls

---
+++ src/url/dto/url.dto.ts
@@ -0,0 +1,47 @@
+import { IsNumber, IsString, IsEnum, IsOptional } from 'class-validator';
+import { UrlAccess } from 'src/generated/prisma/client';
+
+export class CreateUrlDto {
+  @IsString()
+  fullUrl: string;
+
+  @IsEnum(UrlAccess)
+  access: UrlAccess;
+
+  @IsOptional()
+  @IsNumber()
+  groupId?: number;
+}
+
+export class UrlMetaDto {
+  @IsString()
+  shortenedUrl: string;
+  access: UrlAccess;
+  owner: {
+    name: string;
+  };
+}
+
+export class UrlFullDataDto {
+  @IsNumber()
+  id: number;
+
+  @IsOptional()
+  @IsNumber()
+  groupId: number | null;
+
+  @IsString()
+  fullUrl: string;
+
+  @IsString()
+  shortenedUrl: string;
+
+  access: UrlAccess;
+
+  @IsNumber()
+  ownerId: number;
+
+  owner: {
+    name: string;
+  };
+}
